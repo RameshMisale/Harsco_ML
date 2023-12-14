249f2156f94490de40e28191ddab3297f054793b
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-12-14 15:47:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,48 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import joblib
+import base64
+import pickle
+from sklearn.linear_model import LogisticRegression
 
-# Load your trained model 
-model = joblib.load('Harsco_mpdel.pkl')
+# Define function to perform prediction
+def perform_prediction(df):
+    model_case = pickle.load(open('Harsco_mpdel.pkl', 'rb'))
+    # Make predictions on the dataframe
+    y_pred_case = model_case.predict(df)
+    y_pred_case =pd.DataFrame(y_pred_case,columns=['predicted_values'])
+    
+    result_df = pd.concat([df.reset_index(drop=True),pd.DataFrame(y_pred_case, columns=['predicted_values'])], axis=1)
+    
+    return result_df
 
-# Create a Streamlit web app
-st.title("Classification Model Deployment")
+# Set app title and page icon
+st.set_page_config(page_title='CSV File Uploader', page_icon=':open_file_folder:')
 
-# Custom CSS to add a background image
-st.markdown(
-    """
-    <style>
-        body {
-            background-image: url('Logo.png');  # Replace 'your_image_url.jpg' with the URL or local path of your image
-            background-size: cover;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# Set app header
+st.header('CSV File Uploader')
 
-# Allow users to upload a CSV file
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+# Create file uploader component
+csv_file = st.file_uploader('Choose a CSV file', type='csv')
 
-if uploaded_file is not None:
-    try:
-        # Try reading the uploaded CSV file with different encodings
-        for encoding in ['utf-8', 'latin-1', 'ISO-8859-1']:
-            try:
-                df = pd.read_csv(uploaded_file, encoding=encoding)
-                break  # If successful, exit the loop
-            except UnicodeDecodeError:
-                continue  # Try the next encoding if decoding fails
-
-        # Assuming 'Resubmit_binary' is your target variable
-        X = df.drop(columns=['Resubmit'])
-
-        # Make predictions on the target variable
-        y_pred = model.predict(X)
-
-        # Add predictions as a new column
-        df['Prediction'] = y_pred
-
-        # Display the DataFrame with predictions
-        st.subheader("DataFrame with Predictions")
-        st.write(df)
-
-        # Display model evaluation on the uploaded data
-        st.subheader("Model Evaluation on Uploaded Data")
-
-        # Assuming 'Resubmit_binary' is your target variable
-        y_true = df['Resubmit']
-
-        # Evaluate the model
-        accuracy = accuracy_score(y_true, y_pred)
-        classification_report_str = classification_report(y_true, y_pred)
-        confusion_mat = confusion_matrix(y_true, y_pred)
-
-        # Display metrics
-        st.write("Accuracy:", accuracy)
-        st.write("Confusion Matrix:")
-        st.write(confusion_mat)
-        st.write("Classification Report:")
-        st.text(classification_report_str)
-
-    except UnicodeDecodeError:
-        st.error("Error: Unable to decode the CSV file. Please make sure the file is encoded in UTF-8, latin-1, or ISO-8859-1.")
+# Check if a file was uploaded
+if csv_file is not None:
+    df = pd.read_csv(csv_file)
+    st.write(df)
+    if st.button('Perform Prediction'):
+        result_df = perform_prediction(df)
+        st.write(result_df)
+        
+        # Create a button to download the predicted dataframe as a CSV file
+        if 'download_button' not in st.session_state:
+            st.session_state.download_button = False
+        
+        if st.button('Download Predicted CSV File'):
+            st.session_state.download_button = True
+        
+        if st.session_state.download_button:
+            csv = result_df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()
+            href = f'<a href="data:file/csv;base64,{b64}" download="predicted_data.csv">Download Predicted CSV File</a>'
+            st.markdown(href, unsafe_allow_html=True)
+else:
+    st.warning('Please upload a CSV file')
