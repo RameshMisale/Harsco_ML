96ac1eac94d3b655a676baa43be5763c8c0deb36
RameshMisale <85447346+RameshMisale@users.noreply.github.com>
2023-12-14 15:03:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import joblib
 
-# Load your trained model
+# Load your trained model 
 model = joblib.load('Harsco_model.pkl')
 
 # Create a Streamlit web app
